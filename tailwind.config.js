3582b43607cf046440a1b4756491950b8160c113
Dangelo Arrivillaga <dangeloarrivillaga@gmail.com>
2026-01-08 17:17:10 -0400

    Config migration pt. 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
+        "./app/**/*.{js,ts,jsx,tsx}",
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
         "./index.html",
         "./src/**/*.{js,ts,jsx,tsx}",
     ],
